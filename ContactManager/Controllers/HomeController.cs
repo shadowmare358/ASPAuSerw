81532e6b603e4bea5a766422d272f863d008a70c
shadowmare358 <shadowmare@windowslive.com>
2016-10-25 16:51:10 +0200

    zmieniłem 25.10

---
+++ ContactManager/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+           // ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Nasze dane";
+
+            return View();
+        }
+       //[Authorize(Roles = "canEdit")]
+      //  public ActionResult Contacts()
+      //  {
+      //      return View();
+     //   }
+    }
+}
